901904287e53c054708b8224ab2203045a388b1c
rivaldofez <rivaldofez@gmail.com>
2023-12-17 03:14:23 +0700

    implement camera router

--- ChromaFilter/Module/Camera/Router/CameraRouter.swift
+++ ChromaFilter/Module/Camera/Router/CameraRouter.swift
@@ -5,4 +5,31 @@
 //  Created by Rivaldo Fernandes on 17/12/23.
 //
 
-import Foundation
+import UIKit
+
+typealias BeginEntry = CameraRouterProtocol & UIViewController
+
+protocol CameraRouterProtocol {
+    var begin: BeginEntry? { get set }
+    
+    static func start() -> CameraRouterProtocol
+}
+
+class CameraRouter: CameraRouterProtocol {
+    var begin: BeginEntry?
+    
+    static func start() -> CameraRouterProtocol {
+        let router = CameraRouter()
+        
+        var view: CameraViewProtocol = CameraViewController()
+        var presenter: CameraPresenterProtocol = CameraPresenter()
+        
+        view.presenter = presenter
+        presenter.router = router
+        presenter.view = view
+        
+        router.begin = view as? BeginEntry
+        
+        return router
+    }
+}
