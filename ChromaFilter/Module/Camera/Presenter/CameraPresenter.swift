f13b885938a4bcea1a51a8c42a80332f06149b3c
rivaldofez <rivaldofez@gmail.com>
2023-12-17 03:11:31 +0700

    add camera presenter and implement camera presenter class

---
+++ ChromaFilter/Module/Camera/Presenter/CameraPresenter.swift
@@ -0,0 +1,22 @@
+//
+//  CameraPresenter.swift
+//  ChromaFilter
+//
+//  Created by Rivaldo Fernandes on 17/12/23.
+//
+
+import Foundation
+
+protocol CameraPresenterProtocol {
+    var view: CameraViewProtocol? { get set }
+    var router: CameraRouterProtocol? { get set }
+    
+}
+
+class CameraPresenter: CameraPresenterProtocol {
+    var view: CameraViewProtocol?
+    
+    var router: CameraRouterProtocol?
+    
+    
+}
