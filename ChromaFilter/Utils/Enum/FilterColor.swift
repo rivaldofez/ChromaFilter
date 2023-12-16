5b2417d3a8777dbe2a5d81c98b4762a174452bcc
rivaldofez <rivaldofez@gmail.com>
2023-12-17 02:44:27 +0700

    create enum filter color

---
+++ ChromaFilter/Utils/Enum/FilterColor.swift
@@ -0,0 +1,15 @@
+//
+//  FilterColor.swift
+//  ChromaFilter
+//
+//  Created by Rivaldo Fernandes on 17/12/23.
+//
+
+import Foundation
+
+enum FilterColor {
+    case red
+    case green
+    case blue
+    case normal
+}
