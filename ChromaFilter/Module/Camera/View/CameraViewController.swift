7f6dda737572115024f10fa06ad7d2df95aa4cf0
rivaldofez <rivaldofez@gmail.com>
2023-12-17 02:47:05 +0700

    delete default view controller and change to camera view controller

---
+++ ChromaFilter/Module/Camera/View/CameraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraViewController.swift
+//  ChromaFilter
+//
+//  Created by Rivaldo Fernandes on 17/12/23.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
