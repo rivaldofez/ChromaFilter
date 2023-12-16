483da450d1888d2bc4c51a801a8bef2b9e4eca61
rivaldofez <rivaldofez@gmail.com>
2023-12-17 02:48:20 +0700

    create button element in camera view controller

--- ChromaFilter/Module/Camera/View/CameraViewController.swift
+++ ChromaFilter/Module/Camera/View/CameraViewController.swift
@@ -9,6 +9,47 @@ import UIKit
 
 class CameraViewController: UIViewController {
 
+    
+    private let redButton: FilterColorButton = {
+        let button = FilterColorButton()
+        button.filterColor = .red
+        button.layer.cornerRadius = 25
+        button.layer.backgroundColor = UIColor.red.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
+    private let greenButton: FilterColorButton = {
+        let button = FilterColorButton()
+        button.filterColor = .green
+        button.layer.cornerRadius = 25
+        button.layer.backgroundColor = UIColor.green.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
+    private let blueButton: FilterColorButton = {
+        let button = FilterColorButton()
+        button.filterColor = .blue
+        button.layer.cornerRadius = 25
+        button.layer.backgroundColor = UIColor.blue.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
+    private let captureButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 25
+        button.layer.backgroundColor = UIColor.white.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
