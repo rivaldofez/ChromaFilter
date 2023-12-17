9413bb894cbb51fe6dbcc003482450c6e95824aa
rivaldofez <rivaldofez@gmail.com>
2023-12-17 11:33:40 +0700

    implement detail page router

--- ChromaFilter/Module/Detail/Router/DetailRouter.swift
+++ ChromaFilter/Module/Detail/Router/DetailRouter.swift
@@ -17,8 +17,17 @@ class DetailRouter: DetailRouterProtocol {
     var entry: DetailViewController?
     
     static func createDetail(with image: UIImage) -> DetailRouterProtocol {
+        let router = DetailRouter()
         
+        var view: DetailViewProtocol = DetailViewController()
+        var presenter: DetailPresenterProtocol = DetailPresenter()
+        
+        view.presenter = presenter
+        presenter.router = router
+        presenter.view = view
+        presenter.setImage(image: image)
+        
+        router.entry = view as? DetailViewController
+        return router
     }
-    
-    
 }
