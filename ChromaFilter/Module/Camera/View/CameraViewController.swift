7dbc7b18202761f665f622a70aa9ae123e0b8d6d
rivaldofez <rivaldofez@gmail.com>
2023-12-22 22:41:51 +0700

    add bottom stack view

--- ChromaFilter/Module/Camera/View/CameraViewController.swift
+++ ChromaFilter/Module/Camera/View/CameraViewController.swift
@@ -24,7 +24,7 @@ class CameraViewController: UIViewController, CameraViewProtocol {
     private let redButton: FilterColorButton = {
         let button = FilterColorButton()
         button.filterColor = .red
-        button.layer.cornerRadius = 25
+        button.layer.cornerRadius = 30
         button.layer.backgroundColor = UIColor.red.cgColor
         button.layer.borderColor = UIColor.white.cgColor
         
@@ -34,7 +34,7 @@ class CameraViewController: UIViewController, CameraViewProtocol {
     private let greenButton: FilterColorButton = {
         let button = FilterColorButton()
         button.filterColor = .green
-        button.layer.cornerRadius = 25
+        button.layer.cornerRadius = 30
         button.layer.backgroundColor = UIColor.green.cgColor
         button.layer.borderColor = UIColor.white.cgColor
         
@@ -44,17 +44,19 @@ class CameraViewController: UIViewController, CameraViewProtocol {
     private let blueButton: FilterColorButton = {
         let button = FilterColorButton()
         button.filterColor = .blue
-        button.layer.cornerRadius = 25
+        button.layer.cornerRadius = 30
         button.layer.backgroundColor = UIColor.blue.cgColor
         button.layer.borderColor = UIColor.white.cgColor
         
         return button
     }()
     
+   
+    
     private let captureButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 25
+        button.layer.cornerRadius = 30
         button.layer.backgroundColor = UIColor.white.cgColor
         button.layer.borderColor = UIColor.black.cgColor
         
@@ -68,6 +70,14 @@ class CameraViewController: UIViewController, CameraViewProtocol {
         stackView.alignment = .center
         stackView.spacing = 16
         return stackView
+    }()
+    
+    private var bottomView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .gray.withAlphaComponent(0.3)
+        
+        return view
     }()
     
     private lazy var imagePreview: MetalRenderView = {
@@ -97,37 +107,53 @@ class CameraViewController: UIViewController, CameraViewProtocol {
     
     private func configureConstraints() {
         view.addSubview(imagePreview)
-        view.addSubview(captureButton)
-        view.addSubview(filterColorStackView)
+
         filterColorStackView.addArrangedSubview(redButton)
         filterColorStackView.addArrangedSubview(greenButton)
         filterColorStackView.addArrangedSubview(blueButton)
+        filterColorStackView.addArrangedSubview(customButton)
+        
+        view.addSubview(bottomView)
+        bottomView.addSubview(filterColorStackView)
+        bottomView.addSubview(captureButton)
         
         let captureButtonConstraints = [
-            captureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            captureButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            captureButton.heightAnchor.constraint(equalToConstant: 50),
-            captureButton.widthAnchor.constraint(equalToConstant: 50)
+            captureButton.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor),
+            captureButton.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -32),
+            captureButton.heightAnchor.constraint(equalToConstant: 60),
+            captureButton.widthAnchor.constraint(equalToConstant: 60)
         ]
         
         let filterColorStackViewConstraints = [
-            filterColorStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            filterColorStackView.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor),
             filterColorStackView.bottomAnchor.constraint(equalTo: captureButton.topAnchor, constant: -16)
         ]
         
         let redButtonConstraints = [
-            redButton.heightAnchor.constraint(equalToConstant: 50),
-            redButton.widthAnchor.constraint(equalToConstant: 50)
+            redButton.heightAnchor.constraint(equalToConstant: 60),
+            redButton.widthAnchor.constraint(equalToConstant: 60)
         ]
         
         let greenButtonConstraints = [
-            greenButton.heightAnchor.constraint(equalToConstant: 50),
-            greenButton.widthAnchor.constraint(equalToConstant: 50)
+            greenButton.heightAnchor.constraint(equalToConstant: 60),
+            greenButton.widthAnchor.constraint(equalToConstant: 60)
         ]
         
         let blueButtonConstraints = [
-            blueButton.heightAnchor.constraint(equalToConstant: 50),
-            blueButton.widthAnchor.constraint(equalToConstant: 50)
+            blueButton.heightAnchor.constraint(equalToConstant: 60),
+            blueButton.widthAnchor.constraint(equalToConstant: 60)
+        ]
+        
+        let customButtonConstraints = [
+            customButton.heightAnchor.constraint(equalToConstant: 60),
+            customButton.widthAnchor.constraint(equalToConstant: 60)
+        ]
+        
+        let bottomStackViewConstraints = [
+            bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomView.heightAnchor.constraint(equalToConstant: 200)
         ]
         
         NSLayoutConstraint.activate(captureButtonConstraints)
@@ -135,12 +161,15 @@ class CameraViewController: UIViewController, CameraViewProtocol {
         NSLayoutConstraint.activate(redButtonConstraints)
         NSLayoutConstraint.activate(greenButtonConstraints)
         NSLayoutConstraint.activate(blueButtonConstraints)
+        NSLayoutConstraint.activate(customButtonConstraints)
+        NSLayoutConstraint.activate(bottomStackViewConstraints)
     }
     
     private func setFilterButtonAction() {
         redButton.addTarget(self, action: #selector(filterButtonAction), for: .touchUpInside)
         greenButton.addTarget(self, action: #selector(filterButtonAction), for: .touchUpInside)
         blueButton.addTarget(self, action: #selector(filterButtonAction), for: .touchUpInside)
+        customButton.addTarget(self, action: #selector(filterButtonAction), for: .touchUpInside)
         captureButton.addTarget(self, action: #selector(captureImage), for: .touchUpInside)
     }
     
@@ -150,18 +179,27 @@ class CameraViewController: UIViewController, CameraViewProtocol {
                 redButton.layer.borderWidth = 3
                 greenButton.layer.borderWidth = 0
                 blueButton.layer.borderWidth = 0
+                customButton.layer.borderWidth = 0
             case .green:
                 redButton.layer.borderWidth = 0
                 greenButton.layer.borderWidth = 3
                 blueButton.layer.borderWidth = 0
+                customButton.layer.borderWidth = 0
             case .blue:
                 redButton.layer.borderWidth = 0
                 greenButton.layer.borderWidth = 0
                 blueButton.layer.borderWidth = 3
+                customButton.layer.borderWidth = 0
             case .normal:
                 redButton.layer.borderWidth = 0
                 greenButton.layer.borderWidth = 0
                 blueButton.layer.borderWidth = 0
+                customButton.layer.borderWidth = 0
+            case .custom:
+                redButton.layer.borderWidth = 0
+                greenButton.layer.borderWidth = 0
+                blueButton.layer.borderWidth = 0
+                customButton.layer.borderWidth = 3
         }
     }
     
@@ -210,6 +248,6 @@ class CameraViewController: UIViewController, CameraViewProtocol {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        checkCameraPermission()
+//        checkCameraPermission()
     }
 }
